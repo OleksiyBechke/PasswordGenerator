5ddda91b4bfe43b5e1b712a8d70cec6249b60f82
Oleksiy Bechke <bechke@dlit.dp.ua>
2024-09-12 21:56:45 +0300

    Initial commit

---
+++ src/main/java/org/example/app/PasswordGenerator.java
@@ -0,0 +1,22 @@
+package org.example.app;
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+<>?";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public static String generatePassword(int length) {
+        if (length < 1) {
+            throw new IllegalArgumentException("Довжина пароля має бути більше 0");
+        }
+
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomCharIndex = RANDOM.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(randomCharIndex));
+        }
+        return password.toString();
+    }
+}
